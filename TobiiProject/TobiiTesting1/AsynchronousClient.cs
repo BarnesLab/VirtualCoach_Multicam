7ca51a04a4921efd481576783515959d2c2b7ab5
Tianzi Wang <wtianzi@gmail.com>
2019-06-26 18:56:13 -0400

    new version

---
+++ TobiiProject/TobiiTesting1/AsynchronousClient.cs
@@ -0,0 +1,303 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Tobii.Research;
+
+namespace TobiiTesting1
+{
+    public static class AsynchronousClient
+    {
+        // The port number for the remote device.
+        //device id: 023b64
+        //detail refer to http://developer.empatica.com/windows-streaming-server-commands.html
+
+        private const string ServerAddress = "127.0.0.1";
+        private const int ServerPort = 28000;
+
+        // ManualResetEvent instances signal completion.
+        private static readonly ManualResetEvent ConnectDone = new ManualResetEvent(false);
+        private static readonly ManualResetEvent SendDone = new ManualResetEvent(false);
+        private static readonly ManualResetEvent ReceiveDone = new ManualResetEvent(false);
+
+        // The response from the remote device.
+        private static String _response = String.Empty;
+
+        private static Socket client;
+        private static bool m_startsaving;
+        private static string empaticadatasavingpath;
+
+        private static string m_record;//empatica data here
+
+        public static bool StartClient(string str_empaticaDevice = "AB2B64")
+        {
+            // Connect to a remote device.
+            try
+            {
+                m_startsaving = false;
+
+                // Establish the remote endpoint for the socket.
+                var ipHostInfo = new IPHostEntry { AddressList = new[] { IPAddress.Parse(ServerAddress) } };
+                var ipAddress = ipHostInfo.AddressList[0];
+                var remoteEp = new IPEndPoint(ipAddress, ServerPort);
+
+                // Create a TCP/IP socket.
+                //var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // Connect to the remote endpoint.
+                client.BeginConnect(remoteEp, (ConnectCallback), client);
+                ConnectDone.WaitOne();
+
+                /* 
+                 * device_connect 9ff167
+                 * R device_connect OK
+                 * 
+                 * device_subscribe bvp ON
+                 * R device_subscribe bvp OK
+                 * 
+                 * E4_Bvp 123345627891.123 31.128
+                 * 
+                 * device_disconnect
+                 * device_disconnect OK
+                 * 
+                 *  acc - 3-axis acceleration
+                    bvp - Blood Volume Pulse
+                    gsr - Galvanic Skin Response
+                    ibi - Interbeat Interval and Heartbeat
+                    tmp - Skin Temperature
+                    bat - Device Battery
+                    tag - Tag taken from the device (by pressing the button)
+
+                 */
+                string[] list_empatica_device_management_msg = { "device_list", "device_connect "+ str_empaticaDevice }; //AB2B64,9ff167, "device_disconnect"
+
+                string[] list_empatica_datatype_msg ={
+                    "device_subscribe bvp ON",
+                    "device_subscribe gsr ON" };
+
+                /*
+                string [] list_empatica_datatype_msg ={
+                    "device_subscribe acc ON",
+                    "device_subscribe bvp ON",
+                    "device_subscribe gsr ON",
+                    "device_subscribe ibi ON",
+                    "device_subscribe tmp ON",
+                    "device_subscribe bat ON",
+                    "device_subscribe tag ON" };
+                */
+                System.Threading.Thread.Sleep(1000);//2000
+                foreach (string item in list_empatica_device_management_msg)
+                {
+                    Send(client, item + Environment.NewLine);
+                    SendDone.WaitOne();
+                    Receive(client);
+                    ReceiveDone.WaitOne();
+                    System.Threading.Thread.Sleep(500);//2000
+                }
+                foreach (string item in list_empatica_datatype_msg)
+                {
+                    Send(client, item + Environment.NewLine);
+                    SendDone.WaitOne();
+                    Receive(client);
+                    ReceiveDone.WaitOne();
+                    System.Threading.Thread.Sleep(100);//2000
+                }
+
+                /*
+                while (true)
+                {
+                    var msg = Console.ReadLine();
+                    Send(client, msg + Environment.NewLine);
+                    SendDone.WaitOne();
+                    Receive(client);
+                    ReceiveDone.WaitOne();
+                }
+                */
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            return true;
+        }
+        public static void StopClient()
+        {
+            m_startsaving = false;
+            Send(client, "device_disconnect" + Environment.NewLine);
+            SendDone.WaitOne();
+            Receive(client);
+            ReceiveDone.WaitOne();
+        }
+
+
+        public static void SavingRecord(string filepath)
+        {
+            m_startsaving = true;
+            
+            GenerateRecordingFile(filepath);
+        }
+
+        public static void SavingEverySecond()
+        {
+            System.IO.File.AppendAllText(empaticadatasavingpath, m_record);
+            m_record = "";
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                var client = (Socket)ar.AsyncState;
+
+                // Complete the connection.
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint);
+
+                // Signal that the connection has been made.
+                ConnectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Receive(Socket client)
+        {
+            try
+            {
+                // Create the state object.
+                var state = new StateObject { WorkSocket = client };
+
+                // Begin receiving the data from the remote device.
+                client.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, ReceiveCallback, state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                var state = (StateObject)ar.AsyncState;
+                var client = state.WorkSocket;
+
+                // Read data from the remote device.
+                var bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    state.Sb.Append(Encoding.ASCII.GetString(state.Buffer, 0, bytesRead));
+                    _response = state.Sb.ToString();
+
+                    HandleResponseFromEmpaticaBLEServer(_response);
+
+                    state.Sb.Clear();
+
+                    ReceiveDone.Set();
+
+                    // Get the rest of the data.
+                    client.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, ReceiveCallback, state);
+                }
+                else
+                {
+                    // All the data has arrived; put it in response.
+                    if (state.Sb.Length > 1)
+                    {
+                        _response = state.Sb.ToString();
+                    }
+                    // Signal that all bytes have been received.
+                    ReceiveDone.Set();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                
+            }
+        }
+
+        private static void Send(Socket client, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            client.BeginSend(byteData, 0, byteData.Length, 0, SendCallback, client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                var t_client = (Socket)ar.AsyncState;
+                // Complete sending the data to the remote device.
+                t_client.EndSend(ar);
+                // Signal that all bytes have been sent.
+                SendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void HandleResponseFromEmpaticaBLEServer(string response)
+        {
+            Console.Write(response);
+            //transfer unixtimestamp to 
+
+            //var local_timestamp3 = DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff").ToString();
+            //Console.Write(local_timestamp3 + "\r\n");
+            
+
+            if (m_startsaving)
+            {
+                var local_timestamp = DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff").ToString();
+               
+                var unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                Console.Write(local_timestamp +" " + unixTimestamp + "\r\n");
+                var t_str = String.Format("{0},{1}\r\n",
+                    response.Replace(" ",",").Replace("\r\n",""),
+                    local_timestamp);
+                m_record += t_str;
+                //System.IO.File.AppendAllText(empaticadatasavingpath, t_str);
+            }
+            
+            //Console.Write("all here");
+        }
+
+        private static void GenerateRecordingFile(string filepath)
+        {
+            //for eyegazedata
+            //var systemTimeStamp = EyeTrackingOperations.GetSystemTimeStamp();
+            var local_timestamp = DateTimeOffset.Now.ToString("MM_dd_yyyy hh_mm_ss");
+            string t_txtfilename = String.Format("_EP_{0}.txt", local_timestamp);
+
+            //var Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+            //string t_txtfilename= "_" + Timestamp.ToString() + ".txt";
+            empaticadatasavingpath = filepath.Replace(".", "_") + t_txtfilename;
+
+            if (!System.IO.File.Exists(empaticadatasavingpath))
+            {
+                //create file
+                using (var t_file = System.IO.File.Create(empaticadatasavingpath)) ;
+            }
+
+            System.IO.File.WriteAllText(empaticadatasavingpath, "STREAM_TYPE,EP_TIMESTAMP,DATA,TimeStamp\r\n");
+
+        }
+    }
+}
---
+++ TobiiProject/TobiiTesting1/Form1.Designer.cs
@@ -0,0 +1,329 @@
+namespace TobiiTesting1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rfsh = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.bt_trial = new System.Windows.Forms.Button();
+            this.trialIndex = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.debugInstructionsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView_CameraControl = new System.Windows.Forms.ListView();
+            this.Record = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Camera = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Index = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.show = new System.Windows.Forms.Button();
+            this.comboBox_showcameras = new System.Windows.Forms.ComboBox();
+            this.bt_empatica = new System.Windows.Forms.Button();
+            this.timer_empatica = new System.Windows.Forms.Timer(this.components);
+            this.textBox_empatica = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // helloWorldLabel
+            // 
+            this.helloWorldLabel.AutoSize = true;
+            this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldLabel.Location = new System.Drawing.Point(202, 19);
+            this.helloWorldLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.helloWorldLabel.Name = "helloWorldLabel";
+            this.helloWorldLabel.Size = new System.Drawing.Size(131, 26);
+            this.helloWorldLabel.TabIndex = 3;
+            this.helloWorldLabel.Text = "Hello World!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Strat Tobii Eye Tracking";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 545);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(323, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(756, 375);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Camera";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(499, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 313);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rfsh
+            // 
+            this.rfsh.Location = new System.Drawing.Point(400, 86);
+            this.rfsh.Name = "rfsh";
+            this.rfsh.Size = new System.Drawing.Size(75, 23);
+            this.rfsh.TabIndex = 9;
+            this.rfsh.Text = "Refresh";
+            this.rfsh.UseVisualStyleBackColor = true;
+            this.rfsh.Visible = false;
+            this.rfsh.Click += new System.EventHandler(this.rfsh_Click_1);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(67, 386);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(153, 50);
+            this.save.TabIndex = 10;
+            this.save.Text = "Record";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // bt_trial
+            // 
+            this.bt_trial.Location = new System.Drawing.Point(67, 467);
+            this.bt_trial.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_trial.Name = "bt_trial";
+            this.bt_trial.Size = new System.Drawing.Size(153, 45);
+            this.bt_trial.TabIndex = 11;
+            this.bt_trial.Text = "Start A New Trial";
+            this.bt_trial.UseVisualStyleBackColor = true;
+            this.bt_trial.Click += new System.EventHandler(this.bt_trial_Click);
+            // 
+            // trialIndex
+            // 
+            this.trialIndex.Location = new System.Drawing.Point(306, 480);
+            this.trialIndex.Margin = new System.Windows.Forms.Padding(2);
+            this.trialIndex.Name = "trialIndex";
+            this.trialIndex.Size = new System.Drawing.Size(45, 20);
+            this.trialIndex.TabIndex = 12;
+            this.trialIndex.Text = "1";
+            this.trialIndex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(681, 545);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(284, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click here to continue learning how to build a desktop app!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // debugInstructionsLabel
+            // 
+            this.debugInstructionsLabel.AutoSize = true;
+            this.debugInstructionsLabel.Location = new System.Drawing.Point(577, 511);
+            this.debugInstructionsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.debugInstructionsLabel.Name = "debugInstructionsLabel";
+            this.debugInstructionsLabel.Size = new System.Drawing.Size(355, 13);
+            this.debugInstructionsLabel.TabIndex = 1;
+            this.debugInstructionsLabel.Text = "Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 483);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Trial Index:";
+            // 
+            // listView_CameraControl
+            // 
+            this.listView_CameraControl.CheckBoxes = true;
+            this.listView_CameraControl.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Record,
+            this.Camera,
+            this.Index});
+            this.listView_CameraControl.Location = new System.Drawing.Point(69, 136);
+            this.listView_CameraControl.Name = "listView_CameraControl";
+            this.listView_CameraControl.Size = new System.Drawing.Size(321, 97);
+            this.listView_CameraControl.TabIndex = 15;
+            this.listView_CameraControl.UseCompatibleStateImageBehavior = false;
+            this.listView_CameraControl.View = System.Windows.Forms.View.Details;
+            // 
+            // Record
+            // 
+            this.Record.Text = "Record";
+            this.Record.Width = 89;
+            // 
+            // Camera
+            // 
+            this.Camera.Text = "Camera";
+            this.Camera.Width = 165;
+            // 
+            // Index
+            // 
+            this.Index.Text = "Index";
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(400, 136);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 97);
+            this.show.TabIndex = 16;
+            this.show.Text = "Show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // comboBox_showcameras
+            // 
+            this.comboBox_showcameras.FormattingEnabled = true;
+            this.comboBox_showcameras.Location = new System.Drawing.Point(499, 371);
+            this.comboBox_showcameras.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox_showcameras.Name = "comboBox_showcameras";
+            this.comboBox_showcameras.Size = new System.Drawing.Size(240, 21);
+            this.comboBox_showcameras.TabIndex = 17;
+            this.comboBox_showcameras.SelectedIndexChanged += new System.EventHandler(this.comboBox_showcameras_SelectedIndexChanged);
+            // 
+            // bt_empatica
+            // 
+            this.bt_empatica.Location = new System.Drawing.Point(67, 319);
+            this.bt_empatica.Name = "bt_empatica";
+            this.bt_empatica.Size = new System.Drawing.Size(153, 47);
+            this.bt_empatica.TabIndex = 18;
+            this.bt_empatica.Text = "Start Empatica";
+            this.bt_empatica.UseVisualStyleBackColor = true;
+            this.bt_empatica.Click += new System.EventHandler(this.bt_empatica_Click);
+            // 
+            // timer_empatica
+            // 
+            this.timer_empatica.Interval = 5000;
+            this.timer_empatica.Tick += new System.EventHandler(this.timer_empatica_Tick);
+            // 
+            // textBox_empatica
+            // 
+            this.textBox_empatica.Location = new System.Drawing.Point(260, 333);
+            this.textBox_empatica.Name = "textBox_empatica";
+            this.textBox_empatica.Size = new System.Drawing.Size(100, 20);
+            this.textBox_empatica.TabIndex = 19;
+            this.textBox_empatica.Text = "AB2B64";
+            this.textBox_empatica.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 595);
+            this.Controls.Add(this.textBox_empatica);
+            this.Controls.Add(this.bt_empatica);
+            this.Controls.Add(this.comboBox_showcameras);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.listView_CameraControl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trialIndex);
+            this.Controls.Add(this.bt_trial);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.rfsh);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.helloWorldLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.debugInstructionsLabel);
+            this.Controls.Add(this.linkLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button rfsh;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button bt_trial;
+        private System.Windows.Forms.TextBox trialIndex;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label debugInstructionsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView_CameraControl;
+        private System.Windows.Forms.ColumnHeader Record;
+        private System.Windows.Forms.ColumnHeader Camera;
+        private System.Windows.Forms.ColumnHeader Index;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.ComboBox comboBox_showcameras;
+        private System.Windows.Forms.Button bt_empatica;
+        private System.Windows.Forms.Timer timer_empatica;
+        private System.Windows.Forms.TextBox textBox_empatica;
+    }
+}
+
